31bec988fb00073000a1cb2e94f4dccc4c07303d
ibrahimshoshaaa <brahimsosa9@gmail.com>
2026-09-15 18:05:51 +0300

    Add server locale resolver

---
+++ lib/locale-server.ts
@@ -0,0 +1,1 @@
+import {cookies} from 'next/headers';import {LOCALE_COOKIE,type Locale} from './i18n';export async function getLocale():Promise<Locale>{return(await cookies()).get(LOCALE_COOKIE)?.value==='en'?'en':'ar'}
